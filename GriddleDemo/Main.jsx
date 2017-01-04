e4ffd71a18f2bc9d1df99fc59812b6f667fa69a9
williamhhk <william.han@gmail.com>
2017-01-03 17:48:06 -0700

    Add project files.

---
+++ GriddleDemo/Main.jsx
@@ -0,0 +1,30 @@
+﻿class ComponentWithGriddle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedRowId: 0,
+        };
+    }
+    onRowClick(row) {
+        console.log(row);
+        console.log(row.props.data.id);
+        this.setState({ selectedRowId: row.props.data.id });
+    }
+    render() {
+        const rowMetadata = {
+            bodyCssClassName: rowData => (rowData.id === this.state.selectedRowId ? 'selected-red' : ''),
+        };
+
+
+        console.log(rowMetadata);
+        return (
+        <Griddle results={fakeData}  onRowClick={this.onRowClick.bind(this)}  showFilter={true} showSettings={true}  columnMetadata={columnMeta} useFixedHeader={true} rowMetadata={rowMetadata} resultsPerPage={5} enableInfiniteScroll={true} bodyHeight={400 }  useGriddleStyles ={true}
+        />
+        );
+     }
+}
+
+ReactDOM.render(
+    <ComponentWithGriddle />,
+    document.getElementById("griddle-basic")
+);
